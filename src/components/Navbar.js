e9e577a65d3e9f992d02257aba5a1047c7e14f9d
SaadEla <sa.elaouani@gmail.com>
2020-02-24 09:28:14 +0100

    vsCode

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,7 +23,7 @@ function Navbar() {
             </ul>
             <form className="form-inline my-2 my-lg-0">
                 <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">search</button>
             </form>
         </div>
     </nav>
